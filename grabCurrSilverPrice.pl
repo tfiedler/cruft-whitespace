0e7e2707042a66cd92a018c1a05b84e2f8a8e0c8
Ted Fiedler <fiedlert@gmail.com>
2012-09-23 11:10:48 -0400

    moved coins to csv file

--- grabCurrSilverPrice.pl
+++ grabCurrSilverPrice.pl
@@ -4,51 +4,40 @@ use strict;
 use warnings;
 
 use LWP::Simple;
+
+# Expecting name, quantity, % silver, weight in grams, face value, origin country, target country
+my $incsv = $ARGV[0]; 
+
+my %coins;
+
+if ( -f "$incsv" )
+{
+	open my $_fh, $incsv 
+		or die "unable to open $incsv for reading: $!\n";
+	while (<$_fh>)
+	{
+		next if $. == 1;
+		my ($coin, @data) = split /,/;
+		$coin =~ s/"//g;
+		$coins{$coin}{'quantity'} = $data[0];
+		$coins{$coin}{'ps'} = $data[1];
+		$coins{$coin}{'weight'} = $data[2];
+		$coins{$coin}{'fv'} = $data[3];
+		$coins{$coin}{'origin'} = $data[4];
+		$coins{$coin}{'target'} = $data[5];
+	}
+
+	close ($_fh);
+}
+else
+{
+	print "usage: $0 <csv file>\n";
+}
+
 my $content = get("http://www.coinflation.com/silver_coin_values.html");
 die "Couldn't get it!" unless defined $content;
 
-my $ConversionCN = get("http://themoneyconverter.com/CAD/USD.aspx");
-die "Couldn't get it!" unless defined $ConversionCN;
-
-my $CNtoUSD;
-
-if ( $ConversionCN =~ /Latest Exchange Rates: 1 Canadian Dollar = (\d.\d+) United States Dollar/ )
-{
-	$CNtoUSD = $1;
-}
-
 my $cf='.0321507466';
-my %coins;
-
-# ps = % silver, fv = face value ...
-$coins{'franklin half dollar'}{'weight'} = "12.5"; # grams
-$coins{'franklin half dollar'}{'ps'} = ".90"; 
-$coins{'franklin half dollar'}{'fv'} = ".50"; 
-$coins{'franklin half dollar'}{'quantity'} = "0"; 
-$coins{'morgan silver dollar'}{'weight'} = "26.73"; # grams
-$coins{'morgan silver dollar'}{'ps'} = ".90"; 
-$coins{'morgan silver dollar'}{'fv'} = "1.00"; 
-$coins{'morgan silver dollar'}{'quantity'} = 1;
-$coins{'peace dollar'}{'weight'} = "26.73"; # grams
-$coins{'peace dollar'}{'ps'} = ".90"; 
-$coins{'peace dollar'}{'fv'} = "1.00"; 
-$coins{'peace dollar'}{'quantity'} = 1;
-$coins{'american silver eagle'}{'weight'} = "31.10"; # grams
-$coins{'american silver eagle'}{'ps'} = ".999"; 
-$coins{'american silver eagle'}{'fv'} = "1.00"; 
-$coins{'american silver eagle'}{'quantity'} = 3;
-$coins{'washington quarter'}{'weight'} = "6.25"; # grams
-$coins{'washington quarter'}{'ps'} = ".90"; 
-$coins{'washington quarter'}{'fv'} = ".25"; 
-$coins{'washington quarter'}{'quantity'} = 2;
-$coins{'liberty half dollar'}{'weight'} = '12.5'; # grams
-$coins{'liberty half dollar'}{'ps'} = ".90";
-$coins{'liberty half dollar'}{'fv'} = ".50";
-$coins{'liberty half dollar'}{'quantity'} = 1;
-$coins{'queens diamond jubilee'}{'quantity'} = 1;
-$coins{'queens diamond jubilee'}{'ps'} = ".999";
-$coins{'queens diamond jubilee'}{'fv'} = $CNtoUSD * 20.00;
-$coins{'queens diamond jubilee'}{'weight'} = "7.96";
 
 my $valuation = 0;
 my $facevalue = 0;
@@ -70,7 +59,9 @@ if ( $content =~ /silver price for (\w+ \d+, \d\d\d\d): <br><b>Silver<\/b> \$(\d
 		my $value = sprintf("%.4f", $coins{$type}{'weight'} * $cf * $Silver * $coins{$type}{'ps'});
                 my $worth = sprintf("%.2f", $coins{$type}{'quantity'} * $value);
                 $valuation = $valuation + $worth;
-		$facevalue = $facevalue + $coins{$type}{'fv'};
+		$facevalue = $facevalue + 
+			( ConvertTo($coins{$type}{'origin'},$coins{$type}{'target'}) * $coins{$type}{'fv'} );
+
                 my $line = sprintf("%-22s %-4s%8s %8s", $type, $coins{$type}{'quantity'}, $value, $worth);
 		#print "$type, $value, $worth\n";
 		print "$line\n";
@@ -81,4 +72,22 @@ my $bottomline = sprintf("%-15s %.2f %15s %.2f", "Total worth:", $valuation, "Fa
 print "$bottomline\n";
 print "=" x 50 . "\n";
 
+sub ConvertTo
+{
+	my $ORIGIN = shift;
+	my $TARGET = shift;
 
+        return 1 if $ORIGIN = $TARGET;
+
+	my $URL = "http://themoneyconverter.com/${ORIGIN}/${TARGET}.aspx";
+
+	my $get = get($URL);
+	die "Couldn't get it!" unless defined $get;
+
+	my $toUSD;
+	if ( $get =~ /Latest Exchange Rates: 1 \w+ \w+ = (\d\.\d+) United States Dollar/ )
+	{
+        	$toUSD = $1;
+	}
+	return $toUSD;
+}
