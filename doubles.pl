002fde75fa2e73e3516f4f6acf13575dec8484c9
Ted Fiedler <fiedlert@gmail.com>
2013-10-07 11:20:53 -0400

    Update doubles.pl
    fixes

--- doubles.pl
+++ doubles.pl
@@ -8,34 +8,33 @@ use Getopt::Long;
 
 # Iterations allow you to put limits on a series
 my $opts = GetOptions (
-    "iterations=i" => \ my $iterations,
+    "series=i" => \ my $series,
 );
 
 my %thingsIknow;
 
 $|++;
-my $count = 1;
-my $major_iters = 1;
+my $count = 0;
+my $iter = 1;
 while (<STDIN>)
 {
     chomp;
+    $count++;
 
     if ( $thingsIknow{$_}++ ) {
         # quote stuff, if you need to
         my $q = ( $_ =~ /\s+/ ) ? qq|"| : '';
-        print "${major_iters}:${count}:${q}$_${q}\n";
-        #unless ( $count == defined $iterations ) { print "${major_iters}:${count}:${q}$_${q}\n"; }
-        $count = 1;
-        $major_iters++;
+        print "${iter}:${count}:${q}$_${q}\n";
+        $count = 0;
+        $iter++;
         %thingsIknow = ();
     }
-    elsif ( ( defined $iterations > 0 && $count >= $iterations ) ) {
-        $count = 1;
-        $major_iters++;
+    elsif ( defined $series > 0 and ( $count >= $series ) ) {
+        $count = 0;
+        $iter++;
+        %thingsIknow = ();
     }
-
-    $count++;
 
 }
 
-exit 0
+exit 0;
