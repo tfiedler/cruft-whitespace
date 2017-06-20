31548222dd2f982d70c4009918a97cef0dbb37c0
Ted Fiedler <fiedlert@gmail.com>
2017-06-20 17:18:51 +0000

    updated tfiedler/etherpad to use centos 7

--- docker/etherpad/installetherpad.sh
+++ docker/etherpad/installetherpad.sh
@@ -3,7 +3,7 @@
 mkdir /usr/local/etherpad
 cd /usr/local/etherpad
 git clone git://github.com/ether/etherpad-lite.git
-cd etherpad-lite 
-npm install ep_codepad
-npm instal ep_padlist
-npm install ep_expiration
+cd /usr/local/etherpad/etherpad-lite 
+#npm install ep_codepad
+#npm instal ep_padlist
+#npm install ep_expiration
